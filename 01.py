5c864ddf83ddeb1b11c678111d23d1b4132fb926
Yeurch <yeurch@users.noreply.github.com>
2020-12-01 16:34:22 +0000

    Day 01

---
+++ 01.py
@@ -0,0 +1,36 @@
+with open('01a-input.txt', 'r') as f:
+    items = [int(i) for i in f.read().splitlines()]
+
+items.sort()
+
+done = False
+for i in range(len(items)-1):
+    for j in range(i+1, len(items)):
+        sum = items[i] + items[j]
+
+        if sum == 2020:
+            print(f'{items[i]} * {items[j]} = {items[i] * items[j]}')
+            done = True
+            break
+        elif sum > 2020:
+            break
+    if done:
+        break
+
+done = False
+for i in range(len(items)-2):
+    for j in range(i+1, len(items)-1):
+        for k in range(j+1, len(items)):
+            sum = items[i] + items[j] + items[k]
+
+            if sum == 2020:
+                print(f'{items[i]} * {items[j]} * {items[k]} = {items[i] * items[j] * items[k]}')
+                done = True
+                break
+            elif sum > 2020:
+                break
+        if done:
+            break
+    if done:
+        break
+
