1f679f08e33a9b352e4e1a2162ff99caeb82db20
Yeurch <yeurch@users.noreply.github.com>
2020-12-03 16:44:24 +0000

    Ensure both puzzles are output for day 03

--- 03.py
+++ 03.py
@@ -16,7 +16,8 @@ def main():
     test_cases = [(1,1), (3,1), (5,1), (7,1), (1,2)]
     results = [count_trees(items, x, y) for x, y in test_cases]
     print(results)
-    print(functools.reduce(lambda x,y: x*y, results))
+    print(f'1: {results[1]}') # puzzle 1
+    print(f'2: {functools.reduce(lambda x,y: x*y, results)}') # puzzle 2
 
 if __name__ == '__main__':
     main()
