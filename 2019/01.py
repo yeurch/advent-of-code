87a8623e6b6ce94bdac448d151ccc4ed1afedae1
Yeurch <yeurch@users.noreply.github.com>
2020-12-14 10:09:42 +0000

    Day 2019-01

---
+++ 2019/01.py
@@ -0,0 +1,37 @@
+from collections import *
+from functools import lru_cache, reduce
+import heapq
+import itertools
+import math
+import random
+import sys
+import re
+import time
+
+def main():
+    results = []
+
+    with open('01-input.txt', 'r') as f:
+        items = [int(i.strip()) for i in f.read().splitlines()]
+
+    # Part 1
+    results.append(sum([x // 3 - 2 for x in items]))
+
+    # Part 2
+    total_fuel = 0
+    for i in items:
+        fuel = i // 3 - 2
+        last_fuel = fuel
+        while last_fuel > 0:
+            last_fuel = last_fuel // 3 - 2
+            fuel += last_fuel if last_fuel > 0 else 0
+        total_fuel += fuel
+    results.append(total_fuel)
+
+    for i,s in enumerate(results):
+        print(f'{i+1}: {s}')
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    print("--- Executed in {0:.3f} milliseconds ---".format((time.time() - start_time)*1000))
