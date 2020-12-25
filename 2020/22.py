9aa0e692f6f05347ca576a4cdb498427764c44e9
Yeurch <yeurch@users.noreply.github.com>
2020-12-25 19:30:42 +0000

    Refactor day 22 part 1 to use native list rather than SimpleQueue (to simplify part 2)

--- 2020/22.py
+++ 2020/22.py
@@ -1,5 +1,4 @@
 import time
-from queue import SimpleQueue
 
 def main():
     results = []
@@ -7,26 +6,25 @@ def main():
     with open('22-input.txt', 'r') as f:
         input = [i.strip() for i in f.read().splitlines()]
     
-    hands = (SimpleQueue(), SimpleQueue())
+    hands = ([], [])
     p = 0
     for i in [x for x in input if not x.startswith('Player')]:
         if i == '':
             p = 1
             continue
-        hands[p].put(int(i))
-    print(hands)
+        hands[p].append(int(i))
 
-    while not (hands[0].empty() or hands[1].empty()):
-        cards = (hands[0].get(), hands[1].get())
+    while len(hands[0]) > 0 and len(hands[1]) > 0:
+        cards = (hands[0].pop(0), hands[1].pop(0))
         winner = 0 if cards[0] > cards[1] else 1
-        hands[winner].put(cards[winner])
-        hands[winner].put(cards[1-winner])
+        hands[winner].append(cards[winner])
+        hands[winner].append(cards[1-winner])
     
-    winner_hand = hands[0] if hands[1].empty() else hands[1]
-    num_cards = winner_hand.qsize()
+    winner_hand = hands[0] if len(hands[1]) == 0 else hands[1]
+    num_cards = len(winner_hand)
     result = 0
     for i in range(num_cards, 0, -1):
-        result += winner_hand.get() * i
+        result += winner_hand.pop(0) * i
     results.append(result)
 
     for i,s in enumerate(results):
