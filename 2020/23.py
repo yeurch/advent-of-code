f34e60842cbc397aa88bafba386ae3b5d2021dd1
Yeurch <yeurch@users.noreply.github.com>
2020-12-26 17:52:27 +0000

    Day 23 part 2

--- 2020/23.py
+++ 2020/23.py
@@ -1,5 +1,94 @@
 import time
 
+class CircularList:
+    def __init__(self, nodes):
+        node = Node(value=nodes.pop(0))
+        self.__node_dict = {node.value: node}
+        self.head = node
+        for elem in nodes:
+            node.next = Node(value=elem)
+            node = node.next
+            self.__node_dict[elem] = node
+        node.next = self.head
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        first = True
+        while node != self.head or first:
+            first = False
+            nodes.append(str(node.value))
+            node = node.next
+        nodes.append('None')
+        return ' -> '.join(nodes)
+    
+    def __iter__(self):
+        node = self.head
+        first = True
+        while node != self.head or First:
+            first = False
+            yield node
+            node = node.next
+
+    def advance(self):
+        self.head = self.head.next
+    
+    def get(self, n):
+        return self.__node_dict[n]
+    
+    def get_list(self, a, b):
+        node = self.head
+        for i in range(b):
+            if i >= a:
+                yield node
+            node = node.next
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+    def __repr__(self):
+        return str(self.value)
+
+def run_simulation(items, n, part):
+    max_item = max(items)
+    min_item = min(items)
+
+    data = CircularList(items)
+    for i in range(n):
+        target = data.head.value
+        first_four = list(data.get_list(0, 4))
+        first_four_values = [x.value for x in first_four]
+        while target in first_four_values:
+            target -= 1
+            if target < min_item: target = max_item
+        target_node = data.get(target)
+
+        # Move three elements
+        data.head.next = first_four[-1].next
+        first_four[3].next = target_node.next
+        target_node.next = first_four[1]
+
+        # Advance the first element
+        data.advance()
+
+    node = data.get(1).next
+
+    if part == 1:
+        result = []
+        while node.value != 1:
+            result.append(str(node.value))
+            node = node.next
+        
+        return ''.join(result)
+    else:
+        result = 1
+        for i in range(2):
+            result *= node.value
+            node = node.next
+        return result
+
 def main():
     results = []
 
@@ -7,25 +96,12 @@ def main():
         input = [i.strip() for i in f.read()]
     
     items = [int(c) for c in input]
-    print(items)
+    results.append(run_simulation(items, 100, part=1))
 
-    max_item = max(items)
-    min_item = min(items)
-    for i in range(100):
-        target = items[0]
-        while items.index(target) < 4:
-            target -= 1
-            if target < min_item: target = max_item
-        targetpos = items.index(target)
-        items = items[4:targetpos+1] + items[1:4] + items[targetpos+1:] + items[0:1]
-        print(items)
-
-    while items[0] != 1:
-        i = items.pop(0)
+    items = [int(c) for c in input]
+    for i in range(len(items)+1, 1000001):
         items.append(i)
-    results.append(''.join([str(x) for x in items[1:]]))
-    #results.append(0)
-    #results.append(0)
+    results.append(run_simulation(items, 10000000, part=2))
 
     for i,s in enumerate(results):
         print(f'{i+1}: {s}')
