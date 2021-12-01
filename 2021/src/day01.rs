fbd988b9ad0a327566d9028f8dea4266b6c2abca
Yeurch <yeurch@users.noreply.github.com>
2021-12-01 08:41:26 +0000

    Day 01, 2021

--- 2021/src/day01.rs
+++ 2021/src/day01.rs
@@ -1,3 +1,32 @@
 pub fn part1(input: String) {
-    println!("{}", input.chars().rev().collect::<String>());
+    let values: Vec<u64> = input.lines().map(|s| {s.parse().unwrap()} ).collect();
+
+    let mut prev = u64::MAX;
+    let mut result = 0;
+
+    for v in values {
+        if v > prev {
+            result += 1;
+        }
+        prev = v;
+    }
+
+    println!("Part 1 result is {}.", result);
+}
+
+pub fn part2(input: String) {
+    let values: Vec<u64> = input.lines().map(|s| {s.parse().unwrap()} ).collect();
+
+    let mut prev = u64::MAX;
+    let mut result = 0;
+
+    for start in 0 .. values.len() - 2 {
+        let s: u64 = (&values[start..start+3]).iter().sum();
+        if s > prev {
+            result += 1;
+        }
+        prev = s;
+    }
+
+    println!("Part 2 result is {}.", result);
 }
