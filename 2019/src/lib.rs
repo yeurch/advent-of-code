d0a56d164fd75376fd9241ad556ea9713d2de7f7
Yeurch <yeurch@users.noreply.github.com>
2021-12-09 17:58:16 +0000

    Fix unmerged file

--- 2019/src/lib.rs
+++ 2019/src/lib.rs
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-// Days
-pub mod day01;
-=======
 pub mod intcode;
 // Days
 pub mod day01;
 pub mod day02;
 
->>>>>>> origin/master
 
 pub fn noop(_inp: String) {}
 
@@ -16,10 +11,7 @@ pub type DayFn = fn(String);
 pub fn get_day(day: u32) -> (DayFn, DayFn) {
     return match day {
         1 => (day01::part1, day01::part2),
-<<<<<<< HEAD
-=======
         2 => (day02::part1, day02::part2),
->>>>>>> origin/master
         _ => {
             println!("Unknown day: {}", day);
             return (noop, noop);
