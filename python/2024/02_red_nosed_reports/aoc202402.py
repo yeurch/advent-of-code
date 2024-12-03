77f72b260aff1d57044368da58b761a29818ff5b
Yeurch <yeurch@users.noreply.github.com>
2024-12-03 21:07:39 +0000

    Day 02, 2024

---
+++ python/2024/02_red_nosed_reports/aoc202402.py
@@ -0,0 +1,52 @@
+import pathlib
+import sys
+
+def parse_data(puzzle_input):
+    return [tuple([int(number) for number in line.split()]) for line in puzzle_input.split('\n')]
+
+def cmp(a, b):
+    return (a > b) - (a < b)
+
+def is_safe(report):
+    errors_made = 0
+    direction = cmp(report[1], report[0])
+    if direction == 0:
+        return False
+
+    should_abort = False
+    for idx in range(1, len(report)):
+        delta = report[idx] - report[idx - errors_made - 1]
+        if cmp(delta, 0) != direction or abs(delta) < 1 or abs(delta) > 3:
+            return False
+    return True
+
+def part1(data):
+    safe_count = 0
+    for report in data:
+        safe_count += 1 if is_safe(report) else 0
+    return safe_count
+
+def part2(data):
+    safe_count = 0
+    for report in data:
+        if is_safe(report):
+            safe_count += 1
+            continue
+        possible_subreports = [report[:i] + report[i+1:] for i in range(len(report))]
+        for possible_subreport in possible_subreports:
+            if is_safe(possible_subreport):
+                safe_count += 1
+                break
+    return safe_count
+
+def solve(puzzle_input):
+    data = parse_data(puzzle_input)
+    yield part1(data)
+    yield part2(data)
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+            print(f"\n{path}:")
+            solutions = solve(puzzle_input=pathlib.Path(path).read_text().rstrip())
+            print("\n".join(str(solution) for solution in solutions))
+
---
+++ python/2024/02_red_nosed_reports/test_aoc202402.py
@@ -0,0 +1,46 @@
+"""Tests for AoC 1, 2024: Historian Hysteria."""
+
+# Standard library imports
+import pathlib
+
+# Third party imports
+import aoc202402
+import pytest
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+
+@pytest.fixture
+def example1():
+    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().rstrip()
+    return aoc202402.parse_data(puzzle_input)
+
+
+@pytest.fixture
+def example2():
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().rstrip()
+    return aoc202402.parse_data(puzzle_input)
+
+
+def test_parse_example1(example1):
+    """Test that input is parsed properly."""
+    assert example1 == [(7,6,4,2,1),(1,2,7,8,9),(9,7,6,2,1),(1,3,2,4,5),(8,6,4,4,1),(1,3,6,7,9)]
+
+
+def test_part1_example1(example1):
+    """Test part 1 on example input."""
+    assert aoc202402.part1(example1) == 2
+
+
+def test_part2_example1(example1):
+    """Test part 2 on example input."""
+    assert aoc202402.part2(example1) == 4
+
+def test_cmp_big_first():
+    assert aoc202402.cmp(4, 6) == -1
+
+def test_cmp_same():
+    assert aoc202402.cmp(42, 42) == 0
+
+def test_cmp_small_first():
+    assert aoc202402.cmp(6, 3) == 1
