98728670b523be7dbf160a6f2e68aabea753fa88
Yeurch <yeurch@users.noreply.github.com>
2024-12-08 10:38:48 +0000

    Refactor day 08, 2024

--- python/2024/08-resonant-collinearity/aoc202408.py
+++ python/2024/08-resonant-collinearity/aoc202408.py
@@ -23,6 +23,16 @@ def part1(data):
                 antinodes.add(antinode)
     return len([n for n in antinodes if n[0] >= 0 and n[1] >= 0 and n[0] < data.width and n[1] < data.height])
 
+def fire_ray(data, start_pos, delta):
+    antinodes = set()
+    pos = start_pos
+    while True:
+        antinodes.add(pos)
+        pos = (pos[0] + delta[0], pos[1] + delta[1])
+        if pos[0] < 0 or pos[1] < 0 or pos[0] >= data.width or pos[1] >= data.height:
+            break
+    return antinodes
+
 def part2(data):
     frequencies = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
     antinodes = set()
@@ -34,23 +44,9 @@ def part2(data):
                 if other == pos:
                     continue
                 # and iterating over all other antennae of the same frequency
-                vec = (other[0]-pos[0], other[1]-pos[1])
-                antinode = pos
-                while True:
-                    antinodes.add(antinode)
-                    antinode = (antinode[0] - vec[0], antinode[1] - vec[1])
-                    if antinode[0] < 0 or antinode[1] < 0 or antinode[0] >= data.width or antinode[1] >= data.height:
-                        break
-                antinode = other
-                while True:
-                    antinodes.add(antinode)
-                    antinode = (antinode[0] + vec[0], antinode[1] + vec[1])
-                    if antinode[0] < 0 or antinode[1] < 0 or antinode[0] >= data.width or antinode[1] >= data.height:
-                        break
+                antinodes.update(fire_ray(data, pos, (pos[0]-other[0], pos[1]-other[1])))
+                antinodes.update(fire_ray(data, other, (other[0]-pos[0], other[1]-pos[1])))
     return len(antinodes)
-
-    return len([n for n in antinodes if n[0] >= 0 and n[1] >= 0 and n[0] < data.width and n[1] < data.height])
-
 
 def solve(puzzle_input):
     data = parse_data(puzzle_input)
